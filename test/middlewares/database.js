b31d1e27e1052e8453e2d01d1657c469f969a2cf
Lucas <lucas@agtlucas.com>
2014-12-09 23:35:05 -0200

    Update dabase middleware

---
+++ test/middlewares/database.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var co = require('co');
+
+var Models = [
+  mongoose.model('Hub'),
+  mongoose.model('User')
+];
+
+exports.dropDatabase = function (cb) {
+  co(function *() {
+    yield Models.map(dropCollection);
+  })(cb);
+};
+
+var dropCollection = function (Model) {
+  return new Promise(function (resolve, reject) {
+    Model.collection.remove(function (err) {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+};
