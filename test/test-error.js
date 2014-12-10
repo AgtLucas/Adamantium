b7cb0810833472b70054f8c77045fc6bd5f0b12b
Lucas <lucas@agtlucas.com>
2014-12-10 07:57:33 -0200

    Adding test-error

---
+++ test/test-error.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var should = require('should');
+var app = require('../app');
+var request = require('supertest').agent(app.listen());
+
+describe('Errors', function () {
+  it('Should return 404', function (done) {
+    request.get('/urlWhatever')
+      .expect(404)
+      .end(done);
+  });
+});
