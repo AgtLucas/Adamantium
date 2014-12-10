6366fd1f675da1200ad39798d41c56997e389429
Lucas <lucas@agtlucas.com>
2014-12-09 23:42:25 -0200

    Update auth

---
+++ test/middlewares/authenticator.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var should = require('should');
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User');
+
+const CREDENTIALS = {
+  u: 'a@a.com',
+  p: '1234567'
+};
+
+exports.LOGIN_URL = '/login';
+
+exports.createUser = function *() {
+  var user = new User({
+    username: CREDENTIALS.u,
+    password: CREDENTIALS.p
+  });
+};
+
+exports.signAgent = function *(agent, done) {
+  agent
+    .post(exports.LOGIN_URL)
+    .set('Content-Type', 'application/json')
+    .send({ username: CREDENTIALS.u, password: CREDENTIALS.p })
+    .redirects(false)
+    .expect(302)
+    .end(function (err, res) {
+      if (err) done(err);
+      try {
+        res.header.location.should.equal('/');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    })
+};
